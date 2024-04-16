36d1901cbb356e3e999f7a51c7438286365632cb
W-I-W <StarBreak.pixel@gmail.com>
2024-04-16 15:36:50 +0600

    Bow

---
+++ Sudoku/Assets/GameDatabase/Runtime/DataSudoku.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+
+public class DataSudoku
+{
+    public DataSudoku()
+    {
+        m_Grid = new Cell[Row,Column];
+    }
+
+    public const int Row = 9;
+    public const int Column = 9;
+
+    private Cell[,] m_Grid;
+
+    public Cell[,] Create()
+    {
+        for (int x = 0; x < Row; x++)
+        {
+            for (int y = 0; y < Column; y++)
+            {
+                m_Grid[x, y].number = Random.Range(0, 10);
+            }
+        }
+        return m_Grid;
+    }
+}
+
+
